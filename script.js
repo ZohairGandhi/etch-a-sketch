9765a5011ec71e3bf03192d717ab93000da03f1f
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2024-08-10 17:05:07 +1000

    Fix container size regardless of grid dimensions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const CONTAINER_WIDTH = 480;
 let container = document.querySelector(".container");
 let gridDim = 16;
 
@@ -9,6 +10,8 @@ function createGrid() {
         for (let j = 0; j < gridDim; j++) {
             let cellDiv = document.createElement("div");
             cellDiv.setAttribute("class", "cell");
+            let padding = (CONTAINER_WIDTH / gridDim) / 2;
+            cellDiv.style.padding = `${padding}px`;
             colDiv.appendChild(cellDiv);
 
             cellDiv.addEventListener("mouseenter", (e) => handleMouseEnter(e));
